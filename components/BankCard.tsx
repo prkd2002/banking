615c956650b3b0e7178bb4fa810ae58004644651
prkd2002 <prkd2002@gmail.com>
2025-07-18 07:24:21 +0200

    implement the complete layout inklusiv sidebar

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -33,7 +33,7 @@ const BankCard = ({
         </div>
         <div className="bank-card_icon">
           <Image src={"/icons/Paypass.svg"} width={20} height={24} alt="pay" />
-          <Image src={"/icons/mastercard.svg"} width={45} height={32} alt="mastercard" />
+          <Image src={"/icons/mastercard.svg"} width={45} height={32} alt="mastercard" className="ml-5" />
         </div>
         <Image src={"/icons/lines.png"} width={316} height={190} alt="lines" className="absolute top-0 left-0" />
       </Link>
